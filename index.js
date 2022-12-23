c14667adae665c09b579fc24b831bdc9d245b67e
mohammadHidayat <mohammadhidayat8@gmail.com>
2022-12-23 16:17:34 +0600

    mongodb connection added, /setors api for all sectors data created

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.MongoDB_User_Name}:${process.env.MongoDB_User_password}@cluster0.dp8wnte.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const challenge1DataBase = client.db("Challange1");
+    const sectorsCollection = challenge1DataBase.collection("sectors");
+
+    app.get("/sectors", async (req, res) => {
+      const result = await sectorsCollection.find({}).toArray();
+
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
   res.send("Challenge 1 server running");
