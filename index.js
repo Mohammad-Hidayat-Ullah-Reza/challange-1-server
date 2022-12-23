efa45c7fbb581eac5c07387c7e7971c189e9857f
mohammadHidayat <mohammadhidayat8@gmail.com>
2022-12-23 17:32:24 +0600

    /userdata api created for inserting form data

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.MongoDB_User_Name}:${process.env.MongoDB_User_password}@cluster0.dp8wnte.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -20,12 +19,19 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const challenge1DataBase = client.db("Challange1");
+    const challenge1DataBase = client.db("challenge1");
     const sectorsCollection = challenge1DataBase.collection("sectors");
+    const userDataCollection = challenge1DataBase.collection("userData");
 
     app.get("/sectors", async (req, res) => {
       const result = await sectorsCollection.find({}).toArray();
+      res.send(result);
+    });
 
+    app.post("/userData", async (req, res) => {
+      const doc = req.body;
+      console.log(doc);
+      const result = await userDataCollection.insertOne(doc);
       res.send(result);
     });
   } finally {
